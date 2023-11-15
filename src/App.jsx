3f18bd6f1bcf130b370d2a59dcdeca7c43640cc0
Satabdi <satabdiraniweb@gmail.com>
2023-11-15 14:45:31 +0600

    Initialize

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+// import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-5xl bg-green-200 underline'>Vite + React</h1>
+    </>
+  )
+}
+
+export default App
