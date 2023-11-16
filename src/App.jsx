a7f7958daa68c9168ab81b855a35a6805d33ce66
Satabdi <satabdiraniweb@gmail.com>
2023-11-16 12:57:22 +0600

    Details added and bookmark state added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
+import { useState } from "react"
 import Blogs from "./components/Blogs/Blogs"
 import Bookmarks from "./components/Bookmarks/Bookmarks"
 import Header from "./components/Header/Header"
 
 function App() {
+   const [bookmarks, setBookmarks] = useState([]);
+
+   const handleToBookmark = (blog) =>{
+    console.log('adding');
+   }
 
 return (
     <>
       <Header></Header>
-      <div className="md:flex ">
-          <Blogs></Blogs>
+      <div className="md:flex max-w-7xl mx-auto">
+          <Blogs handleToBookmark={handleToBookmark}></Blogs>
           <Bookmarks></Bookmarks>
       </div>
     </>
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -2,8 +2,9 @@
 
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../Blog/Blog";
 
-const Blogs = () => {
+const Blogs = ({handleToBookmark}) => {
     const [blogs, setblogs] = useState([]);
 
     useEffect(() => {
@@ -15,6 +16,13 @@ const Blogs = () => {
     return (
         <div className="md:w-2/3">
             <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+            {
+                blogs.map (blog => <Blog 
+                    key={blog.id} 
+                    blog={blog}
+                    handleToBookmark = {handleToBookmark}>
+                    </Blog>)
+            }
         </div>
     );
 };
